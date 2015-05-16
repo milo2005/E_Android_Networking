024119ae2a20f5c73169c9211e702caac780bf78
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-05-15 19:19:31 -0500

    Consumo de servicio web con asynctask

--- app/src/main/java/com/universidadcauca/movil/clima/MainActivity.java
+++ app/src/main/java/com/universidadcauca/movil/clima/MainActivity.java
@@ -1,20 +1,16 @@
 package com.universidadcauca.movil.clima;
 
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 
-import com.universidadcauca.movil.clima.net.HttpThread;
+import com.universidadcauca.movil.clima.net.HttpTask;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
-public class MainActivity extends ActionBarActivity implements HttpThread.HttpThreadI {
+public class MainActivity extends ActionBarActivity implements HttpTask.HttpTaskI {
 
     String url ="https://query.yahooapis.com/v1/public/yql";
     String urlData="q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20%28select%20woeid%20from%20geo.places%281%29%20where%20text%3D%22popayan%2C%20co%22%29&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
@@ -32,25 +28,12 @@ public class MainActivity extends ActionBarActivity implements HttpThread.HttpTh
         presion = (TextView) findViewById(R.id.txt_pres);
         humedad = (TextView) findViewById(R.id.txt_hum);
 
+        HttpTask task = new HttpTask(this, url, HttpTask.GET);
+        task.execute(urlData);
 
 
-        HttpThread thread = new HttpThread(this,url,urlData,HttpThread.GET);
-        thread.start();
     }
 
-    Handler handler = new Handler(){
-
-        @Override
-        public void handleMessage(Message msg) {
-            switch(msg.what){
-                case HttpThread.GET:
-                    String rta = (String) msg.obj;
-                    processWeather(rta);
-                    break;
-            }
-        }
-
-    };
 
     private void processWeather(String rta) {
         try {
@@ -84,7 +67,7 @@ public class MainActivity extends ActionBarActivity implements HttpThread.HttpTh
 
 
     @Override
-    public Handler getHandler() {
-        return handler;
+    public void setResult(String rta) {
+        processWeather(rta);
     }
 }
